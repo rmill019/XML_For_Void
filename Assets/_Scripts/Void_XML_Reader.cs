9ce5ba27b448ead33b93585a8c708586f8c1b826
rmill019 <rmill013@gmail.com>
2017-10-17 21:38:08 -0400

    Adds XML Parsing

--- Assets/_Scripts/Void_XML_Reader.cs
+++ Assets/_Scripts/Void_XML_Reader.cs
@@ -7,14 +7,49 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Void_XML_Reader : MonoBehaviour {
+	
+	private Text xmlText;
+	public string nameToFind;
+
+	private StringBuilder output = new StringBuilder ();
 
 	// Use this for initialization
 	void Start () {
-		
+		// Grabbing the Text UI object in Unity
+		xmlText = GameObject.Find ("xmlText").GetComponent<Text> ();
+		xmlText.text = " ";
+
+
+		FindByName (nameToFind);
+	}
+
+	// Keep as void or return a string?
+	void FindByName (string cName)
+	{
+		string nameToPrint = "Not found";
+
+		XmlTextReader culpritReader = new XmlTextReader ("Assets\\Resources\\culprits.xml");
+		XmlWriterSettings ws = new XmlWriterSettings ();
+		XmlWriter writer = XmlWriter.Create (output, ws);
+
+		culpritReader.MoveToContent ();
+
+		while (culpritReader.Read ())
+		{
+			culpritReader.ReadToDescendant ("culprit");
+			culpritReader.ReadToFollowing ("name");
+			nameToPrint = culpritReader.ReadElementContentAsString ();
+			if (nameToPrint == cName)
+			{
+				xmlText.text += nameToPrint;
+				break;
+			} 
+			else
+			{
+				culpritReader.Skip ();
+			}
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
