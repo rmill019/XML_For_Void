a37bda310d9e11661a3e2f06c5eb7eaa5460fb2b
rmill019 <rmill013@gmail.com>
2017-10-18 20:04:41 -0400

    Add 20 Boxes to test XMLReader Class

--- Assets/_Scripts/Void_XML_Reader.cs
+++ Assets/_Scripts/Void_XML_Reader.cs
@@ -8,27 +8,38 @@ using UnityEngine.UI;
 
 public class Void_XML_Reader : MonoBehaviour {
 	
-	private Text xmlText;
+	public Text xmlText;
+	public int idToSearchFor;
 	public string nameToFind;
+	public string xmlFileToRead;
 
 	private StringBuilder output = new StringBuilder ();
 
 	// Use this for initialization
 	void Start () {
 		// Grabbing the Text UI object in Unity
-		xmlText = GameObject.Find ("xmlText").GetComponent<Text> ();
+		//xmlText = GameObject.Find ("xmlText").GetComponent<Text> ();
 		xmlText.text = " ";
 
-
-		FindByName (nameToFind);
+		//FindByName (idToSearchFor);
+		Culprit targetCulprit = ReturnCulpritInfo (idToSearchFor);
+		xmlText.text += "ID: " + targetCulprit.id + "\n" +
+						"Name: " + targetCulprit.name + "\n" +
+						"Title: " + targetCulprit.title + "\n" +
+						"Age: " + targetCulprit.age + "\n" +
+						"Description: " + targetCulprit.description + "\n";
 	}
 
-	// Keep as void or return a string?
-	void FindByName (string cName)
-	{
-		string nameToPrint = "Not found";
 
-		XmlTextReader culpritReader = new XmlTextReader ("Assets\\Resources\\culprits.xml");
+
+	// Keep as void or return a string?
+	void FindByName (int cID)
+	{
+		int idToPrint = 0;
+		//string nameToPrint = "Not found";
+
+
+		XmlTextReader culpritReader = new XmlTextReader ("Assets\\Resources\\" + xmlFileToRead);
 		XmlWriterSettings ws = new XmlWriterSettings ();
 		XmlWriter writer = XmlWriter.Create (output, ws);
 
@@ -37,11 +48,12 @@ public class Void_XML_Reader : MonoBehaviour {
 		while (culpritReader.Read ())
 		{
 			culpritReader.ReadToDescendant ("culprit");
-			culpritReader.ReadToFollowing ("name");
-			nameToPrint = culpritReader.ReadElementContentAsString ();
-			if (nameToPrint == cName)
+			culpritReader.ReadToFollowing ("id");
+			idToPrint = culpritReader.ReadElementContentAsInt ();
+			if (idToPrint == cID)
 			{
-				xmlText.text += nameToPrint;
+				int myId = culpritReader.ReadElementContentAsInt ();
+				//xmlText.text += idToPrint.ToString();
 				break;
 			} 
 			else
@@ -49,7 +61,52 @@ public class Void_XML_Reader : MonoBehaviour {
 				culpritReader.Skip ();
 			}
 		}
-	}
-	
 
+		culpritReader.Close ();
+	}
+
+
+
+	Culprit ReturnCulpritInfo (int idNumber)
+	{
+		int intToCompare = 0;
+		//Declare new culprit that will be returned once filled out with relevant data
+		Culprit culpritToReturn = new Culprit ();
+
+		XmlReader culpritReader = XmlReader.Create ("Assets\\Resources\\" + xmlFileToRead);
+		XmlWriterSettings ws = new XmlWriterSettings ();
+		XmlWriter writer = XmlWriter.Create (output, ws);
+
+		culpritReader.MoveToContent();
+
+		while (culpritReader.Read ())
+		{
+			culpritReader.ReadToDescendant ("culprit");
+			culpritReader.ReadToFollowing ("id");
+			// If the id in the xml matches the given parameter argument then gather information to 
+			// stated culprit to return.
+			intToCompare = culpritReader.ReadElementContentAsInt();
+			if (intToCompare == idNumber)
+			{
+				// Messed up by trying to call culpritReader.ReadElementContentAsInt() again and it didn't work. Why not?
+				culpritToReturn.id = intToCompare;
+				culpritReader.ReadToNextSibling ("name");
+				culpritToReturn.name = culpritReader.ReadElementContentAsString ();
+				culpritReader.ReadToNextSibling ("title");
+				culpritToReturn.title = culpritReader.ReadElementContentAsString ();
+				culpritReader.ReadToNextSibling ("age");
+				culpritToReturn.age = culpritReader.ReadElementContentAsInt ();
+				culpritReader.ReadToNextSibling ("description");
+				culpritToReturn.description = culpritReader.ReadElementContentAsString ();
+				break;
+			}
+			else
+			{
+				culpritReader.Skip ();
+			}
+			
+		}
+		culpritReader.Close ();
+		return culpritToReturn;
+	}
 }
